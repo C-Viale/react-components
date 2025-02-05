6f07505643ad152eb37d06bc608bacfbc05b72f9
Caio Lima <caiovialedl@gmail.com>
2025-02-04 23:29:03 -0300

    update button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,13 @@
 import { classNames } from "@functions/classNames";
-import { capitalize } from "@functions/text";
+import { renderIcon } from "@functions/renderIcon";
 import React, { ReactNode } from "react";
-import { buttonLoadingVariants, buttonSizes, buttonVariants } from "./config";
-import Throbber from "./throbber";
-import type { ButtonVariant, ButtonWidth, IconType, ThrobberPosition } from "./types";
+import "./styles.css";
+import type {
+  ButtonVariant,
+  ButtonWidth,
+  IconType,
+  ThrobberPosition,
+} from "./types";
 
 interface ButtonProps extends React.ComponentPropsWithRef<"button"> {
   rightIcon?: IconType | ReactNode;
@@ -12,9 +16,8 @@ interface ButtonProps extends React.ComponentPropsWithRef<"button"> {
   throbberPosition?: ThrobberPosition;
   variant?: ButtonVariant;
   width?: ButtonWidth;
-  label?: string;
   children?: never;
-  shortcut?: string;
+  label?: string;
 }
 
 export function Button({
@@ -27,57 +30,42 @@ export function Button({
   throbberPosition = "middle",
   type,
   label,
-  shortcut,
   ...props
 }: ButtonProps) {
-  const renderIcon = (icon?: IconType | ReactNode) => {
-    if (!icon) return null;
-
-    if (typeof icon === "function") {
-      const IconComponent = icon as IconType;
-      return <IconComponent className="w-5" />;
-    }
-
-    return icon;
-  };
-
   const showLeftThrobber = loading && throbberPosition === "left";
   const showMiddleThrobber = loading && throbberPosition === "middle";
   const showRightThrobber = loading && throbberPosition === "right";
 
   return (
     <button
-      className={classNames(
-        "h-fit rounded-lg self-end relative",
-        "disabled:bg-neutral-200 disabled:text-gray-400 disabled:cursor-not-allowed disabled:active:outline-none disabled:ring-0",
-        "active:outline-info-600 active:outline",
-        "h-10 px-4 py-2",
-        "transition whitespace-nowrap",
-        buttonSizes[width],
-        buttonVariants[variant],
-        loading && buttonLoadingVariants[variant],
-        loading && "pointer-events-none",
-        className,
-      )}
       type={type ?? "button"}
+      className={classNames(
+        "cvl-button",
+        `cvl-button-${variant}`,
+        {
+          "cvl-button-full": width === "full",
+          "cvl-button-loading": loading,
+        },
+        className
+      )}
       {...props}
     >
       {showMiddleThrobber && (
-        <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-          <Throbber variant={variant} />
+        <div className="cvl-button-throbber-wrapper">
+          <div className="cvl-button-throbber" />
         </div>
       )}
       <div
-        className={classNames("flex justify-center items-center gap-2 whitespace-nowrap", {
-          "opacity-0": showMiddleThrobber,
-        })}
+        className={classNames(
+          "cvl-button-content",
+          showMiddleThrobber && "cvl-button-content-hidden"
+        )}
       >
-        {showLeftThrobber && <Throbber variant={variant} />}
-        {renderIcon(leftIcon)}
-        {label && <span className="text-ellipsis overflow-hidden ">{capitalize(label.trim(), false)}</span>}
-        {shortcut && <span>[ {shortcut} ]</span>}
-        {renderIcon(rightIcon)}
-        {showRightThrobber && <Throbber variant={variant} />}
+        {showLeftThrobber && <div className="cvl-button-throbber" />}
+        {leftIcon && renderIcon(leftIcon)}
+        {label && <span className="cvl-button-label">{label?.trim()}</span>}
+        {rightIcon && renderIcon(rightIcon)}
+        {showRightThrobber && <div className="cvl-button-throbber" />}
       </div>
     </button>
   );
--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,7 +1,4 @@
 export type IconType = React.ComponentType<{ className?: string }>;
-
-export type ButtonVariant = "primary" | "secondary" | "tertiary" | "danger";
-
+export type ButtonVariant = "primary" | "danger";
 export type ThrobberPosition = "left" | "middle" | "right";
-
 export type ButtonWidth = "fit" | "full";
